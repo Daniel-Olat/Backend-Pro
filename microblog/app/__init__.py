b77e9daa8b16fb400b2561b9adb9795833e08243
Daniel Olatunji <danielolatunji2907@gmail.com>
2025-12-21 00:56:58 +0100

    Handling Login Forms

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,5 +1,5 @@
-from flask import Flask 
-
+from flask import Config, Flask 
+from config import config
 app = Flask(__name__)
-
+app.config.from_object(config) #Importing the config file for use by the flask server.
 from app import routes
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,5 @@
 from app import app
 from flask import render_template
-
 # view functions
 @app.route('/')
 @app.route('/home')
@@ -17,3 +16,9 @@ def index():
         }
     ] # Mock object for a system of users 
     return render_template('home.html', title='Home', user=user , posts=posts)
+
+from app.forms import LoginForm 
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template("login.html" , title = "SIGN IN" , form=form)
