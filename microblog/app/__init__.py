fa002c5dd48db463c4972de61c3da4893182e8c0
Daniel Olatunji <danielolatunji2907@gmail.com>
2025-12-22 12:47:19 +0100

    Initial database schema implemented

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,5 +1,9 @@
-from flask import Config, Flask 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from config import config
+
 app = Flask(__name__)
-app.config.from_object(config) #Importing the config file for use by the flask server.
-from app import routes
+app.config.from_object(config)  # Importing the config file for use by the flask server.
+db = SQLAlchemy(app)
+
+from app import routes, models
