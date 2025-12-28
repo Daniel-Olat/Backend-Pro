6b9ebfcf57d0c9cb1e82fc19fc025f7fca7a572e
Daniel Olatunji <danielolatunji2907@gmail.com>
2025-12-28 17:44:02 +0100

    Django blog

--- microblog/app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm 
-from wtforms import StringField , PasswordField , BooleanField, SubmitField 
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-  username = StringField('username' , validators = [DataRequired()]) #validators required to ensure data is entered
-  password = PasswordField('password' , validators = [DataRequired()])
-  remember_me = BooleanField("Remember Me")
-  submit = SubmitField('Sign In')
